cede920ab21c2fc7c144c9055dcd00b2c19572bf
nsaeger1 <nsaeger1@my.wctc.edu>
2021-05-14 16:40:30 -0500

    Initial commit

---
+++ AwesomeApp.Android/obj/Debug/100/android/typemap.mj.x86.s
@@ -0,0 +1,25 @@
+	/* Data Hash: 58103504446A5D04 */
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	7191
+	/* entry-length */
+	.long	266
+	/* value-offset */
+	.long	145
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 1912807
+	.include	"typemap.mj.inc"
---
+++ AwesomeApp/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AwesomeApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+      
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            int drinkAge = 21;
+            
+            DateTime birth = Picker.Date;
+            TimeSpan age = DateTime.Today.Subtract(birth);
+            int year21 = age.Days / 365;
+            int day21 = age.Days%365;
+            Lbl_BMI.Text = $"You have {year21} years, and  {day21 - (year21/4)} days until you can drink.";
+        }
+    }
+}
